39ae47673513a7e254834e584c3dcd5071b451e8
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-06 15:22:21 -0600

    App.js: provides basic routing for testing; initializes firebase app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
 "use strict";
 
 angular.module("DocApp", ["ngRoute"])
+// Is this constant necessary if FirebaseCredentials has the URL?
 .constant("FBUrl", "https://davidlarsketch-8da73.firebaseio.com/fe-cap")
-.config("")
+.config($routeProvider =>
+  $routeProvider
+  .when('/login', {
+    templateUrl: "assets/partials/login",
+    controller: "LoginCtrl"
+  })
+  .when('/team-login', {
+    templateUrl: "assets/partials/team-login",
+    controller: "TeamLoginCtrl",
+    resolve: {isAuth}
+  })
+  .otherwise('/login')
+)
+.run(FirebaseCredentials =>
+  firebase.initializeApp(FirebaseCredentials)
+);
