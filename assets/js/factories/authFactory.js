8b77b449b95e53609fa201bfa0de5441c7cc7a53
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-06 15:24:23 -0600

    AuthFactory.js: for email/password method, defines login, logout, register, sending user display name to database, & firebase.auth().onAuthStateChanged listener used in app.js isAuth()

---
+++ assets/js/factories/authFactory.js
@@ -0,0 +1,29 @@
+"use strict";
+
+angular.module("DocApp").factory("AuthFactory", function($q, $http, FirebaseCredentials) {
+
+  const isAuthenticated = () =>
+    $q((resolve, reject) =>
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          // From copy-pasta - do I need it?
+          // currentUser = user.uid;
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      }));
+
+  const login = ({email, password}) =>
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(({code, message}) => {return {code, message};});
+
+  const logout = () =>
+    firebase.auth().signOut();
+
+  const registerEmail = ({email, password}) =>
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(({code, message}) => {return {code, message};});
+
+  return {login, logout, registerEmail, isAuthenticated};
+});
