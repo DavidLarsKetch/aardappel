946272164081c50469a81ef272028bddf433b4db
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-07 15:25:57 -0600

    TeamFactory: allows posting new team to database; attaches firebase ID to the data, updating the database obj with that firebase ID for later use

--- assets/js/factories/teamFactory.js
+++ assets/js/factories/teamFactory.js
@@ -1,5 +1,20 @@
 "use strict";
 
 angular.module("DocApp").factory("TeamFactory", function($q, $http, FirebaseCredentials) {
+  const attachFirebaseID = firebaseID =>
+    $q((resolve, reject) =>
+      $http.patch(`${FirebaseCredentials.databaseURL}/teams/${firebaseID}.json`, JSON.stringify({firebaseID}))
+      .then(({data}) => resolve(data))
+      .catch(err => reject(err))
+    );
 
+  const postTeam = postData =>
+    $q((resolve, reject) =>
+      $http.post(`${FirebaseCredentials.databaseURL}/teams.json`, JSON.stringify(postData))
+      .then(({data}) => attachFirebaseID(data.name))
+      .then(({firebaseID}) => resolve(firebaseID))
+      .catch(err => reject(err))
+    );
+
+  return {getTeams, getTeam, patchTeam, postTeam};
 });
