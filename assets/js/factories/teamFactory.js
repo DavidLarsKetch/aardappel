e1e036cb406fd1dc23b2caa2eae5085c2368726c
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-07 15:33:42 -0600

    Fixes #8, TeamFactory: provides necessary HTTP calls for getting teams & enabling a user to add themselves to an existing team

--- assets/js/factories/teamFactory.js
+++ assets/js/factories/teamFactory.js
@@ -8,6 +8,27 @@ angular.module("DocApp").factory("TeamFactory", function($q, $http, FirebaseCred
       .catch(err => reject(err))
     );
 
+  const getTeams = () =>
+    $q((resolve, reject) =>
+      $http.get(`${FirebaseCredentials.databaseURL}/teams.json`)
+      .then(({data}) => resolve(data))
+      .catch(err => reject(err))
+    );
+
+  const getTeam = id =>
+    $q((resolve, reject) =>
+      $http.get(`${FirebaseCredentials.databaseURL}/teams/${id}.json`)
+      .then(({data}) => resolve(data))
+      .catch(err => reject(err))
+    );
+
+  const patchTeam = (id, data) =>
+    $q((resolve, reject) =>
+      $http.patch(`${FirebaseCredentials.databaseURL}/teams/${id}.json`, JSON.stringify(data))
+      .then(({data}) => resolve(data))
+      .catch(err => reject(err))
+    );
+
   const postTeam = postData =>
     $q((resolve, reject) =>
       $http.post(`${FirebaseCredentials.databaseURL}/teams.json`, JSON.stringify(postData))
