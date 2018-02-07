437d6b56fadc5a0789105fef16dbd68d9dcff800
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-07 15:30:20 -0600

    Fixes #16, TeamRegisterCtrl: inserting info into team-register.html & clicking submit button, controller checks whether password is valid, posts new team to database, & routes user to that new team's all-docs view

--- assets/js/controllers/teamRegisterCtrl.js
+++ assets/js/controllers/teamRegisterCtrl.js
@@ -1,5 +1,21 @@
 "use strict";
 
-angular.module("DocApp").controller("TeamRegisterCtrl", function($scope) {
+angular.module("DocApp").controller("TeamRegisterCtrl", function($scope, $location, TeamFactory) {
   $scope.test = "Sup, TeamRegisterCtrl";
+  $scope.team = {
+    displayName: '',
+    password: '',
+    users: [firebase.auth().currentUser.uid],
+    owner: firebase.auth().currentUser.uid
+  };
+
+  $scope.registerTeam = () => {
+    if($scope.team.password === $scope.reenter)
+    // TODO: Check for whether team name already exists, deny registration if so
+      TeamFactory.postTeam($scope.team)
+      .then(teamID => {
+        console.log(teamID);
+        $location.path(`/docs/${teamID}`);
+      });
+  };
 });
