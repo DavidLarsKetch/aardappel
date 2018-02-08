cf06780c99140034838b79589fc6ff3b3162fd5b
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-08 13:49:00 -0600

    Fixes #40: Grabs team data from TeamFactory & checks whether current uid matches any uid in users prop; if not, redirects to teams-login view

--- assets/js/controllers/allDocsCtrl.js
+++ assets/js/controllers/allDocsCtrl.js
@@ -1,5 +1,16 @@
 "use strict";
 
-angular.module("DocApp").controller("AllDocsCtrl", function($scope) {
+angular.module("DocApp").controller("AllDocsCtrl", function($scope, $location, $routeParams, TeamFactory) {
   $scope.test = "Sup, AllDocsCtrl";
+  const uid = firebase.auth().currentUser.uid;
+  //Verifies user has access to team
+  TeamFactory.getTeam($routeParams.team_id)
+  .then(teamData => {
+    if (teamData.users.includes(uid)) {
+      $scope.team = teamData;
+    } else {
+      $location.path('/team-login');
+    }
+  })
+  .catch(err => console.log(err));
 });
