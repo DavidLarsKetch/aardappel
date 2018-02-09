af0cc790fdf1291939e77c259edddb185709349e
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-09 11:28:29 -0600

    UserFactory: Gets user data for uid passed in

--- assets/js/factories/userFactory.js
+++ assets/js/factories/userFactory.js
@@ -1,6 +1,11 @@
 "use strict";
 
 angular.module("DocApp").factory("UserFactory", function($q, $http, FirebaseCredentials) {
+  // For drilling down past Firebase ID as key for the obj
+  const grabFirebaseID = data => Object.keys(data);
+  
+  // Firebase Authentication saves user data, but need separate obj for
+  // custom properties used in app
   const registerDisplayName = data =>
     $q((resolve, reject) =>
       $http.post(`${FirebaseCredentials.databaseURL}/users.json`, JSON.stringify(data))
@@ -8,5 +13,12 @@ angular.module("DocApp").factory("UserFactory", function($q, $http, FirebaseCred
       .catch(err => reject(err))
     );
 
-  return {registerDisplayName};
+    const getUser = uid =>
+      $q((resolve, reject) =>
+        $http.get(`${FirebaseCredentials.databaseURL}/users.json?orderBy="uid"&equalTo="${uid}"`)
+        .then(({data}) => resolve(data[grabFirebaseID(data)]))
+        .catch(err => reject(err))
+      );
+
+  return {getUser, registerDisplayName};
 });
