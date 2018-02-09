427a3ff8c72a73a584f460bf12951ff35f2a63b5
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-09 15:33:44 -0600

    Fixes #11, #12, #13: ReviewPendingCtrl - user can mark docs as completed review, saving their uid as 'reviewer' prop on doc; AllDocsCtrl - reviewer displayName attached to doc; DocFactory - allows get single doc & updating doc in db with put

--- assets/js/factories/docFactory.js
+++ assets/js/factories/docFactory.js
@@ -13,22 +13,36 @@ angular.module("DocApp").factory("DocFactory", function($q, $http, FirebaseCrede
       $http.post(`${FirebaseCredentials.databaseURL}/docs.json`, JSON.stringify(data))
       .then(({data}) => attachFirebaseID(data.name))
       .then(({firebaseID}) => resolve(firebaseID))
-      .catch(err => console.log(err))
+      .catch(err => reject(err))
     );
 
   const deleteDoc = id =>
     $q((resolve, reject) =>
       $http.delete(`${FirebaseCredentials.databaseURL}/docs/${id}.json`)
       .then(() => resolve())
-      .catch(err => console.log(err))
+      .catch(err => reject(err))
+    );
+
+  const getDoc = id =>
+    $q((resolve, reject) =>
+      $http.get(`${FirebaseCredentials.databaseURL}/docs/${id}.json`)
+      .then(({data}) => resolve(data))
+      .catch(err => reject(err))
     );
 
   const getDocs = id =>
     $q((resolve, reject) =>
       $http.get(`${FirebaseCredentials.databaseURL}/docs.json?orderBy="team_id"&equalTo="${id}"`)
       .then(({data}) => resolve(data))
-      .catch(err => console.log(err))
+      .catch(err => reject(err))
     );
 
-  return {postDoc, deleteDoc, getDocs};
+  const putDoc = doc =>
+    $q((resolve, reject) =>
+      $http.put(`${FirebaseCredentials.databaseURL}/docs/${doc.firebaseID}.json`, JSON.stringify(doc))
+      .then(() => resolve())
+      .catch(err => reject(err))
+    );
+
+  return {postDoc, putDoc, deleteDoc, getDoc, getDocs};
 });
