069551b95f5aba698ed2ab6afaf353535faf1708
David Lars Ketch <davidlarsketch@gmail.com>
2018-02-21 23:09:54 -0600

    Styling: title-logout. Follows BEM. Fixes 'Switch Teams' button display

--- app/shared/title-logout/titleLogoutCtrl.js
+++ app/shared/title-logout/titleLogoutCtrl.js
@@ -3,12 +3,19 @@
 angular.module("DocApp").controller("TitleLogoutCtrl",
 function(
   $scope,
-  NavServices,
+  BoolServices, NavServices,
   AuthFactory
 ) {
-
+  // TODO: Pull title from somewhere else
   $scope.appTitle = "Doc Editor";
+
+  // Checks whether all-docs partial is being shown in order to display
+  // 'Switch Teams' btn, looks at a variable only existing on AllDocsCtrl
+  $scope.checkIfAllDocs = variable => BoolServices.notUndefined(variable);
+
+
   $scope.switchTeams = () => NavServices.go.toTeamsLogin();
+
   $scope.logout = () =>
     AuthFactory.logout()
     .then(() => NavServices.go.toLogin());
